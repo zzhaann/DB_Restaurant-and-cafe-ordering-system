845cb34e4569e151bec2e80c8630b30bbe7c9654
zzhaann <152423777+zzhaann@users.noreply.github.com>
2025-03-05 20:24:40 +0500

    Update OrderingSystem.sql

--- OrderingSystem.sql
+++ OrderingSystem.sql
@@ -111,3 +111,98 @@ CREATE TABLE OrderHistory (
     status VARCHAR(50) CHECK (status IN ('pending', 'preparing', 'ready', 'completed', 'canceled')) NOT NULL,
     change_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+
+--рөл құру
+create role data_manager_role;
+create role order_manager_role;
+create role waiter_role;
+create role courier_role;
+create role customer_role;
+-- құрылған рөлдерді қөру
+select rolname from pg_roles
+where rolname in ('data_manager_role', 'order_manager_role', 'waiter_role','courier_role','customer_role')  ;
+
+--привилигия беру
+GRANT CREATE ON DATABASE order_system TO data_manager_role;
+GRANT CREATE ON SCHEMA public TO data_manager_role;
+grant all privileges on all tables in schema public to data_manager_role;
+grant select, insert, update, delete on orders, tablereservations, orderdetails to order_manager_role;
+grant select, insert on  Orders, MenuItems to waiter_role;
+grant select, update on Deliveries, Orders to courier_role;
+grant SELECT, INSERT on MenuItems, Orders, Reviews to customer_role;
+
+--қолданушы құру
+create user data_manager with password '1234Aa';
+create user order_manager with password '1234Aa';
+create user waiter with password '1234Aa';
+create user courier with password '1234Aa';
+create user customer with password '1234Aa';
+
+--қолданушыға рөлдерді беру
+grant data_manager_role to data_manager;
+grant order_manager_role to order_manager;
+grant waiter_role to waiter;
+grant courier_role to courier;
+grant customer_role to customer;
+ALTER DATABASE postgres OWNER TO data_manager;
+
+--рөл және олардың қолданушылары
+SELECT u.rolname AS user_name, r.rolname AS role_name
+FROM pg_catalog.pg_roles AS u
+LEFT JOIN pg_catalog.pg_auth_members AS m ON u.oid = m.member
+LEFT JOIN pg_catalog.pg_roles AS r ON m.roleid = r.oid
+where r.rolname in ('data_manager_role', 'order_manager_role', 'waiter_role','courier_role','customer_role')
+ORDER BY user_name;
+
+
+--привилигия көру үшін
+SELECT grantee AS user_name,
+       STRING_AGG(privilege_type, ', ') AS privileges,
+       STRING_AGG(table_name, ', ') AS tables
+FROM information_schema.role_table_grants
+WHERE grantee IN ('data_manager_role', 'order_manager_role', 'waiter_role', 'courier_role', 'customer_role')
+GROUP BY grantee
+ORDER BY user_name;
+
+
+-- барлық шектеулерді шығару үшін
+SELECT conname AS constraint_name,
+       contype AS constraint_type,
+       conrelid::regclass AS table_name
+FROM pg_constraint
+ORDER BY table_name, constraint_type;
+
+
+--check
+SELECT conname AS constraint_name,
+       conrelid::regclass AS table_name,
+       pg_get_constraintdef(oid) AS search_condition
+FROM pg_constraint
+WHERE contype = 'c'
+ORDER BY table_name;
+
+
+
+--праймари кейлер
+SELECT conname AS constraint_name,
+       conrelid::regclass AS table_name
+FROM pg_constraint
+WHERE contype = 'p'
+ORDER BY table_name;
+
+--форейн кейс
+SELECT conname AS constraint_name,
+       conrelid::regclass AS table_name
+FROM pg_constraint
+WHERE contype = 'f'
+ORDER BY table_name;
+
+--unique
+SELECT conname AS constraint_name,
+       conrelid::regclass AS table_name
+FROM pg_constraint
+WHERE contype = 'u'
+ORDER BY table_name;
